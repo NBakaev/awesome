d2e3c8fb7699aa36d799bd5d5467f217875200db
Nikita Bakaev <ya@nbakaev.ru>
2016-04-27 18:10:05 +0300

    basic image ubuntu

---
+++ common/ubuntu_image/simple_ubuntu_starter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# add docker repository
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo deb https://apt.dockerproject.org/repo ubuntu-xenial main >> /etc/apt/sources.list.d/docker.list
+apt-get update
+
+# install docker & dnsmasq
+apt-get install docker-engine bridge-utils dnsmasq -y
+
+# enable swap with 4gb
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+free -m
+echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
