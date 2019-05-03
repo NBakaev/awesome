d7c34e680e37b6748a3b12690db2fcc343b3de62
Nikita Bakaev <NBakaev@users.noreply.github.com>
2019-05-03 17:30:39 +0300

    add docker-compose

--- common/ubuntu_image/simple_ubuntu_starter.sh
+++ common/ubuntu_image/simple_ubuntu_starter.sh
@@ -5,7 +5,7 @@ set -e
 
 # add docker repository
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common cifs-utils yum -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
@@ -22,10 +22,14 @@ free -m
 echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
 
 # micro - text editor https://github.com/zyedidia/micro
-EXPORT MICRO_VERSION=1.4.1
+MICRO_VERSION=1.4.1
 wget https://github.com/zyedidia/micro/releases/download/v${MICRO_VERSION}/micro-${MICRO_VERSION}-linux64.tar.gz
 tar -xvf micro-${MICRO_VERSION}-linux64.tar.gz
 cp micro-${MICRO_VERSION}/micro /bin
 rm -rm ./micro-${MICRO_VERSION}
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 echo "### DONE "###"
