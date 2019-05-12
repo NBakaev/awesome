e093dc21fd339b2a17b1f861b8c3285133a86d09
Nikita Bakaev <NBakaev@users.noreply.github.com>
2019-05-12 09:48:03 +0300

    delete dnsmasq - use systemd; add zsh aliases

--- common/ubuntu_image/simple_ubuntu_starter.sh
+++ common/ubuntu_image/simple_ubuntu_starter.sh
@@ -10,8 +10,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 
-# install docker & dnsmasq
-apt install docker-ce bridge-utils dnsmasq curl openssh-server unzip httpie socat -y
+# install docker
+apt install docker-ce bridge-utils curl openssh-server unzip httpie socat -y
 
 # enable swap with 4gb
 sudo fallocate -l 4G /swapfile
@@ -22,6 +22,7 @@ free -m
 echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
 
 # micro - text editor https://github.com/zyedidia/micro
+echo "Installing micro - text editor..."
 MICRO_VERSION=1.4.1
 wget https://github.com/zyedidia/micro/releases/download/v${MICRO_VERSION}/micro-${MICRO_VERSION}-linux64.tar.gz
 tar -xvf micro-${MICRO_VERSION}-linux64.tar.gz
@@ -29,7 +30,12 @@ cp micro-${MICRO_VERSION}/micro /bin
 rm -rm ./micro-${MICRO_VERSION}
 
 # docker-compose
+echo "Installing docker-compose..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+echo "Creating zsh aliases..."
+echo "\nalias d='docker'" >> $HOME/.zshrc
+echo "\nalias k='kubectl'" >> $HOME/.zshrc
 
 echo "### DONE "###"
