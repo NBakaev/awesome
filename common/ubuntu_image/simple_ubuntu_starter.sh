13b15948bb62f932419af7312bceda7bee849951
Nikita Bakaev <NBakaev@users.noreply.github.com>
2020-12-31 21:52:30 +0300

    Update simple_ubuntu_starter.sh

--- common/ubuntu_image/simple_ubuntu_starter.sh
+++ common/ubuntu_image/simple_ubuntu_starter.sh
@@ -7,7 +7,10 @@ set -e
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common cifs-utils yum -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 sudo apt update
 
 # install docker
@@ -23,15 +26,16 @@ echo "/swapfile   none    swap    sw    0   0" >> /etc/fstab
 
 # micro - text editor https://github.com/zyedidia/micro
 echo "Installing micro - text editor..."
-MICRO_VERSION=1.4.1
+MICRO_VERSION=2.0.8
 wget https://github.com/zyedidia/micro/releases/download/v${MICRO_VERSION}/micro-${MICRO_VERSION}-linux64.tar.gz
 tar -xvf micro-${MICRO_VERSION}-linux64.tar.gz
 cp micro-${MICRO_VERSION}/micro /bin
 rm -rf ./micro-${MICRO_VERSION}
 
 # docker-compose
+DOCKER_COMPOSE_VERSION=1.27.4
 echo "Installing docker-compose..."
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Creating zsh aliases..."
